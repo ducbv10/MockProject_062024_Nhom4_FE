a801b4f6ed40285a37956ae4e974b1ab06735f4b
Quốc Dũng <nguyenquocdung26032003@gmail.com>
2024-07-09 14:42:33 +0700

    feat/add-login-userNavbar-ui-NguyenQuocDung (#1)

--- src/pages/Client/Home.tsx
+++ src/pages/Client/Home.tsx
@@ -1,6 +1,12 @@
-function Home() {
+import UserNavbar from "../../components/Navbar";
+
+
+const Home = () => {
     return (
-        <div><h1>Home Hello world</h1></div>
+        <div>
+          <h1>Home Hello world</h1>
+          <UserNavbar />
+        </div>
     )
 }
 
