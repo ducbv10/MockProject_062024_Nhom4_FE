61b016726dc8a98fb6333ddc4d98f0256b759c93
cyber141 <cyberzzz2k3@gmail.com>
2024-07-10 18:16:21 +0700

    Func: carousel

--- src/pages/Client/Home.tsx
+++ src/pages/Client/Home.tsx
@@ -3,8 +3,14 @@ import Banner from "../../components/Banner";
 import UserNavbar from "../../components/Navbar";
 import Footer from "../../layouts/Client/FooterClient";
 import Header from "@/layouts/Client/HeaderClient";
+import CarouselPros from "@/components/HomePage/CarouselPros";
+import AuctionedProducts from "@/components/HomePage/AuctionedProducts";
+import CurrentAuctionPro from "@/components/HomePage/CurrentAuctionPro";
 
 const Home = () => {
+
+
+
     return (
         <div>
             <Header/>
@@ -13,9 +19,20 @@ const Home = () => {
             <div className="m-4">
                 <UpcomingAuctions />
             </div>
+
+            <div className="m-12">
+                <AuctionedProducts/>
+            </div>
+
+            <div className="m-12">
+                <CurrentAuctionPro/>
+            </div>
+
             <Footer />
         </div>
     )
 }
 
 export default Home;
+
+
