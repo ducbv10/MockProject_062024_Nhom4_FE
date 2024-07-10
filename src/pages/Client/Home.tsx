a5f78bd5661ad91f68dd7c553de0569453781a8b
Quoctuphan <quoctup01@gmail.com>
2024-07-10 14:35:30 +0700

    Push heađer into Homepage

--- src/pages/Client/Home.tsx
+++ src/pages/Client/Home.tsx
@@ -2,10 +2,12 @@ import UpcomingAuctions from "@/components/UpcomingAuctions";
 import Banner from "../../components/Banner";
 import UserNavbar from "../../components/Navbar";
 import Footer from "../../layouts/Client/FooterClient";
+import Header from "@/layouts/Client/HeaderClient";
 
 const Home = () => {
     return (
         <div>
+            <Header/>
             <UserNavbar />
             <Banner />
             <div className="m-4">
