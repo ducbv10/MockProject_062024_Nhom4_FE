7fa9e835e0accbf9eb46756e2d6c10213a28a7ed
cyber141 <cyberzzz2k3@gmail.com>
2024-07-10 15:48:48 +0700

    Forget Passworđ

--- src/pages/Client/Home.tsx
+++ src/pages/Client/Home.tsx
@@ -1,6 +1,10 @@
+import ForgetPass from "../../components/Form/login/ForgetPass";
 function Home() {
     return (
-        <div><h1>Home Hello world</h1></div>
+        <div>
+            <h1>Home Hello world</h1>
+            <ForgetPass/>
+        </div>
     )
 }
 
